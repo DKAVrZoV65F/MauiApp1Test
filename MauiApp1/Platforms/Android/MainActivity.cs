3ce0a25c33d79c826597e3979cad07642dc283cb
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2024-02-10 20:16:42 +0000

    Testing tests

--- MauiApp1/Platforms/Android/MainActivity.cs
+++ MauiApp1/Platforms/Android/MainActivity.cs
@@ -6,5 +6,6 @@ namespace MauiApp1
 {
     [Activity(Theme = "@style/Maui.SplashTheme", ScreenOrientation = ScreenOrientation.Portrait,  MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
-    {}
+    {
+    }
 }
---
+++ TestError/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestError
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
